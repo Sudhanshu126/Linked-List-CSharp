2d5c3c9ee0ef286a558f4c73a00249e665a6d65f
Sudhanshu Kr. Suman <unknown.what.443@gmail.com>
2024-01-12 22:19:28 +0530

    added required C# files

---
+++ Node.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace MyCustomDataStructure
+{
+    //Data struct to store custom data in the Node class
+    struct Data
+    {
+        public string name;
+
+        //Data constructor
+        public Data(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    //Generic Node class to store data as a singly linked list
+    class Node<T> where T : struct
+    {
+        //Data
+        public T data;
+
+        //Link
+        public Node<T>? next;
+
+        //Static properties
+        public static int Size { get; private set; }
+        public static Node<T>? Head { get; private set; }
+
+        //Node constructor
+        public Node(T data, Node<T>? nextNode)
+        {
+            this.data = data;
+            next = nextNode;
+        }
+
+        //Creates a new node with null link
+        public static Node<T> Create(T data)
+        {
+            Node<T>? newNode = new Node<T>(data, null);
+            if (Size == 0)
+            {
+                Node<T>.Head = newNode;
+            }
+            Size++;
+            return newNode;
+        }
+
+        //Finds the end of the linked list
+        public static Node<T>? FindEnd()
+        {
+            Node<T>? startNode;
+            if (Head == null)
+            {
+                return null;
+            }
+            startNode = Head;
+            while (startNode.next != null)
+            {
+                startNode = startNode.next;
+            }
+
+            return startNode;
+        }
+
+        //Inserts a new node at the beginning
+        public static void InsertAtStart(Node<T> newNode)
+        {
+            if(Head != null)
+            {
+                newNode.next = Head;
+            }
+            Head = newNode;
+        }
+
+        //Inserts a new node at the end
+        public static void InsertAtEnd(Node<T> newNode)
+        {
+            Node<T>? endNode = FindEnd();
+            if (endNode == null)
+            {
+                Create(newNode.data);
+            }
+            else
+            {
+                endNode.next = newNode;
+            }
+        }
+
+        //Inserts a new node after a specific node
+        public static void InsertAfter(int nthNode, Node<T> newNode)
+        {
+            Node<T>? refNode = Head;
+            
+            if(refNode ==  null)
+            {
+                Create(newNode.data);
+                return;
+            }
+            if(FindNode(nthNode, out refNode))
+            {
+                newNode.next = refNode?.next;
+                refNode.next = newNode;
+            }
+        }
+
+        //Finds nth node
+        public static bool FindNode(int nthNode, out Node<T>? node)
+        {
+            if (nthNode > Size - 1)
+            {
+                node = FindEnd();
+                return false;
+            }
+            else
+            {
+                Node<T>? refNode = Head;
+                while (nthNode > 0)
+                {
+                    refNode = refNode?.next;
+                    nthNode--;
+                }
+                node =  refNode;
+                return true;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MyCustomDataStructure;
+
+class Program
+{
+    public static void Main()
+    {
+        //Create a data
+        Data data1 = new Data();
+        data1.name = "Shubham";
+        //Creates a node
+        Node<Data> node1 = Node<Data>.Create(data1);
+        //Creates second node
+        Node<Data> node2 = Node<Data>.Create(new Data("Sudhanshu"));
+
+        //Connect first node to second
+        node1.next = node2;
+
+        Node<Data>.InsertAfter(0, Node<Data>.Create(new Data("Kush")));
+        Node<Data>.InsertAtEnd(Node<Data>.Create(new Data("Raushan")));
+
+        if (Node<Data>.FindNode(2, out Node<Data>? foundNode))
+        {
+            Console.WriteLine(foundNode?.data.name);
+        }
+
+        Console.WriteLine(Node<Data>.Size);
+    }
+}
